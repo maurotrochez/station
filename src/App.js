2851a63b9e05b16e462c6a78be13ca61862abe73
David Trochez <maurotrochez@gmail.com>
2016-11-02 21:05:43 -0500

    add react-chartjs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,54 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+var LineChart = require("react-chartjs").Line;
+
+var MyComponent = React.createClass({
+  render: function() {
+    function rand(min, max, num) {
+          var rtn = [];
+          while (rtn.length < num) {
+            rtn.push((Math.random() * (max - min)) + min);
+          }
+          return rtn;
+        }
+    var chartData = {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [
+                {
+                    label: "My First dataset",
+                    fillColor: "rgba(220,220,220,0.2)",
+                    strokeColor: "rgba(220,220,220,1)",
+                    pointColor: "rgba(220,220,220,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(220,220,220,1)",
+                    data: [12,56,23,54]
+                },
+                {
+                    label: "My Second dataset",
+                    fillColor: "rgba(151,187,205,0.2)",
+                    strokeColor: "rgba(151,187,205,1)",
+                    pointColor: "rgba(151,187,205,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(151,187,205,1)",
+                    data: rand(32, 100, 7)
+                }
+            ]
+          };
+    
+    var chartOptions = {
+        scales: {
+            xAxes: [{
+                type: 'linear',
+                position: 'bottom'
+            }]
+        }
+    };
+    return <LineChart data={chartData} options={chartOptions} width="600" height="250"/>
+  }
+});
 
 class App extends Component {
   render() {
@@ -13,9 +61,16 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Chart
+        </p>
+        <MyComponent/>
       </div>
+      
     );
+    
   }
+  
 }
 
 export default App;
